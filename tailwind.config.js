5108e369ee6e26321e3ea7b671417fa34de76cf2
Miguelangel <mestevezo23@gmail.com>
2023-06-18 03:20:00 -0400

    feat: base del proyecto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        jade: "#05C46B",
+      },
+      button: {
+        customButton: {
+          display: "inline-block",
+          lineHeight: "1.5",
+          textAlign: "center",
+          textDecoration: "none",
+          verticalAlign: "middle",
+          cursor: "pointer",
+          "-webkit-user-select": "none",
+          "-moz-user-select": "none",
+          "-ms-user-select": "none",
+          userSelect: "none",
+          backgroundColor: "transparent",
+          borderColor: "transparent",
+          padding: ".375rem .75rem",
+          fontSize: ".875rem",
+          borderRadius: ".375rem",
+        },
+        buttonPrimary: {
+          color: "#FFFFFF",
+          backgroundColor: "#23DD87",
+          borderColor: "#23DD87",
+          padding: "0.45rem 0.8rem",
+          fontSize: "0.875rem",
+        },
       },
     },
   },
   plugins: [],
-}
+};
